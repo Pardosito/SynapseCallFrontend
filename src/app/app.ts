8dc46179eb7e9d9e4ab123e6fb28cbab20cbacb3
alan80avalos <alejando.rodriguez@iteso.mx>
2026-03-27 20:00:39 -0600

    feat: add pages of the landing

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PublicFooter } from "./layouts/public-footer/public-footer";
+import { PublicHeader } from "./layouts/public-header/public-header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PublicFooter, PublicHeader],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -3,10 +3,11 @@ import { PublicHeader } from '../../layouts/public-header/public-header';
 import { PublicFooter } from '../../layouts/public-footer/public-footer';
 import { FeaturesOverview } from './features-overview/features-overview';
 import { PricingTiers } from '../organization/pricing-tiers/pricing-tiers';
+import { HeroSection } from "./hero-section/hero-section";
 
 @Component({
   selector: 'app-landing',
-  imports: [PublicHeader, FeaturesOverview, PricingTiers, PublicFooter],
+  imports: [PublicHeader, FeaturesOverview, PricingTiers, PublicFooter, HeroSection],
   templateUrl: './landing.html',
   styleUrl: './landing.scss',
 })
