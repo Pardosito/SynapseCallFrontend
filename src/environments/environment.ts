9fb4fc8accc2062da7c978f4007471cb24050a19
Pardosito <ianrdzwong@gmail.com>
2026-05-01 20:27:01 -0600

    chore: remove environment files from tracking and add to gitignore

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-	production: true,
-	apiUrl: 'http://localhost:3000',
-	googleClientId: '733781226859-uisv83ut96mfj1icd8j1u8glsrlosk94.apps.googleusercontent.com',
-	paypalClientId: '',
-};
